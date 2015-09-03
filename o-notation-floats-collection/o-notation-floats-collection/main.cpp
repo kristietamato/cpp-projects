3c346e9c7c4d19afae7d7fc00c338d9cbbe48f51
Kristie <kristietamato@gmail.com>
2015-09-02 22:11:11 -0700

    Create the floatCollection class

--- o-notation-floats-collection/o-notation-floats-collection/main.cpp
+++ o-notation-floats-collection/o-notation-floats-collection/main.cpp
@@ -1,13 +1,34 @@
 //  main.cpp
-//  Created by Kristie Nguyen on 9/2/15.
+//  Created by Kristie Nguyen on 9/15.
 //  o-notation-floats-collection
 
 #include <iostream>
 #include <string>
-#include"tools.h"
+#include "tools.h"
 using namespace std;
 
 int main(void)
 {
+    try
+    {
+        
+    }
+    catch (exception e)
+    {
+        cout << "Error" << endl;
+    }
     return 0;
 }
+
+class floatCollection
+{
+    public:
+        float getFloat( void );
+        floatCollection( float theFloat );
+        floatCollection( const floatCollection &obj);
+        ~floatCollection();
+    
+    private:
+    float *floatPointer;
+
+};
