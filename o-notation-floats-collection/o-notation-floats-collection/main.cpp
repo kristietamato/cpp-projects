d0088b98e9df93b2bb14658a72064056c27741d8
Kristie <kristietamato@gmail.com>
2015-09-02 19:55:23 -0700

    Add premade tools.cpp and tools.h

--- o-notation-floats-collection/o-notation-floats-collection/main.cpp
+++ o-notation-floats-collection/o-notation-floats-collection/main.cpp
@@ -1,15 +1,13 @@
-//
 //  main.cpp
-//  o-notation-floats-collection
-//
 //  Created by Kristie Nguyen on 9/2/15.
-//  Copyright (c) 2015 Kristie Nguyen. All rights reserved.
-//
+//  o-notation-floats-collection
 
 #include <iostream>
+#include <string>
+#include"tools.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(void)
+{
     return 0;
 }
