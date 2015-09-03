65f2f9b6ec3da9ad56283e65cd5997adf98b2ee0
Kristie <kristietamato@gmail.com>
2015-09-02 19:40:11 -0700

    Initial commit; create Xcode project with main.cpp

---
+++ o-notation-floats-collection/o-notation-floats-collection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  o-notation-floats-collection
+//
+//  Created by Kristie Nguyen on 9/2/15.
+//  Copyright (c) 2015 Kristie Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
